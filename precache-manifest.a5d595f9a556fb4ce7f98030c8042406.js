c6914f3599e27e67748c643ef068ff49db10d30a
rvld <pattipeilohyterry@gmail.com>
2020-03-30 10:45:19 +0800

    Updates

---
+++ precache-manifest.a5d595f9a556fb4ce7f98030c8042406.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72473c1ca7e53c6daf6a9ccc2fb792ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71c1d0da5aaabbfed879",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "321b3c32ed12326adf4a",
+    "url": "/static/js/2.7db981ae.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7db981ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71c1d0da5aaabbfed879",
+    "url": "/static/js/main.157ecdef.chunk.js"
+  },
+  {
+    "revision": "bd57f4ab45272dd81eb3",
+    "url": "/static/js/runtime-main.0862b134.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
